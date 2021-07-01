2370f3c06fd200b77065a7c621c2cb051f4953ac
matt <mattpalms@gmail.com>
2021-07-01 09:37:36 +0100

    Tweaks to appearance, make grow array more robust.
      * Grow by a multiplier to avoid frequent resizes
      * Reduce the multiplier on each grow to avoid allocating
        too much when it is larger.

--- src/net/byteseek/swing/treetable/NodeDisplayList.java
+++ src/net/byteseek/swing/treetable/NodeDisplayList.java
@@ -1,18 +1,20 @@
 package net.byteseek.swing.treetable;
 
 import java.util.AbstractList;
-import java.util.Arrays;
 import java.util.List;
 
 class NodeDisplayList extends AbstractList<TreeTableNode> {
 
-    private TreeTableNode[] displayedNodes = new TreeTableNode[128];
+    private static final int DEFAULT_CAPACITY = 256;
+    
+    private int growMultiplierPercent = 200;
+    private TreeTableNode[] displayedNodes = new TreeTableNode[DEFAULT_CAPACITY];
     private int size;
 
     public int size() {
         return size;
     }
-
+    
     public TreeTableNode get(int index) {
         checkIndex(index);
         return displayedNodes[index];
@@ -57,7 +59,7 @@ class NodeDisplayList extends AbstractList<TreeTableNode> {
             final int numToAdd = nodes.size();
             checkResize(numToAdd);
             final int numToShift = size - index;
-            // Shift the others along one by the number of nodes:
+            // Shift the others along by the number of nodes:
             for (int position = 0; position < numToShift; position++) {
                 displayedNodes[size + position] = displayedNodes[index + position];
             }
@@ -97,7 +99,6 @@ class NodeDisplayList extends AbstractList<TreeTableNode> {
     }
 
     public void clear() {
-        Arrays.fill(displayedNodes, null); // clear all references to any nodes.
         size = 0;
     }
 
@@ -109,14 +110,32 @@ class NodeDisplayList extends AbstractList<TreeTableNode> {
 
     private void checkResize(int numToAdd) {
         if (size + numToAdd >= displayedNodes.length) {
-            resizeArray();
+            growArray();
         }
     }
 
-    private void resizeArray() {
-        TreeTableNode[] newArray = new TreeTableNode[displayedNodes.length + 128];
+    private void growArray() {
+        if (displayedNodes.length == Integer.MAX_VALUE) {
+            throw new OutOfMemoryError("Cannot increase the list size beyond Integer.MAX_VALUE: " + this);
+        }
+        TreeTableNode[] newArray = new TreeTableNode[getGrowSize()];
         System.arraycopy(displayedNodes, 0, newArray, 0, displayedNodes.length);
         displayedNodes = newArray;
+    }
+    
+    private int getGrowSize() {
+        // We grow by a multiplier that gradually reduces.
+        // This is because we want to grow faster when we're smaller (to avoid frequent re-sizes if large
+        // numbers of nodes are being added), but to grow proportionally a bit slower once we're getting quite big
+        // (or we will waste a lot of space on average).
+        long newSize = displayedNodes.length * growMultiplierPercent / 100;
+        if (growMultiplierPercent > 100) {
+            growMultiplierPercent -= 20; // the next time we grow, it will be by a smaller proportion (more absolute).
+        }
+        if (newSize > Integer.MAX_VALUE) {
+            newSize = Integer.MAX_VALUE;
+        }
+        return (int) newSize;
     }
 
 }
