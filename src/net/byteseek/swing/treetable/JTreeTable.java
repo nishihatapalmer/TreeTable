b9849299b245dfbc811dbccb16e791b220e631e3
matt <mattpalms@gmail.com>
2021-07-01 10:38:05 +0100

    Minor tweaks.

--- src/net/byteseek/swing/treetable/JTreeTable.java
+++ src/net/byteseek/swing/treetable/JTreeTable.java
@@ -1,12 +1,11 @@
 package net.byteseek.swing.treetable;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 //TODO: so far there is nothing we have to do here - we could just use a default JTable... might be nice
-//      to do everything in the model, node and renderer.
+//      to do everything in the model, node and renderer.  Horribly breaks MVC I guess, but honestly,
+//      there isn't really anything to do here we can't do elsewhere.  Nice to be able to just drop a standard
+//      JTable on a form in a visual designer, then bind the model to it and suddenly have a tree table.
 
 public class JTreeTable extends JTable {
 
